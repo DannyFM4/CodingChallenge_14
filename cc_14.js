bc931831488092c71aa363e567327a5ad0ab881a
Daniel Feliciano-Munoz <danielfelicianomunoz@usf.edu>
2025-03-09 17:18:22 -0400

    Task 3 - Highlighting High Priority Tickets

--- cc_14.js
+++ cc_14.js
@@ -8,6 +8,7 @@ function buildSupportTicket(name, issue, priority) { // creates a functon
     const ticket = document.createElement("div");
     ticket.setAttribute("class", "ticketClass");
 
+
     // declares the writing inside the ticket
     ticket.innerHTML = `<h2>${name}</h2><p>${issue}</p><h4>${priority}</h4>`;
 
@@ -19,6 +20,11 @@ function buildSupportTicket(name, issue, priority) { // creates a functon
     //adds resolve button to the html
     ticket.appendChild(resolveButton);
 
+    // task 3: using an if statement to give all high prio tickets a different class name
+    if (priority === "High") {
+        ticket.classList.add("highPriority");
+    }
+
     //adds the tickets to the container
     ticketContainer.appendChild(ticket);
 }
@@ -27,3 +33,14 @@ function buildSupportTicket(name, issue, priority) { // creates a functon
 buildSupportTicket("Barry Bonds", "Computer wont connect to the WiFi", "Medium");
 buildSupportTicket("Hank Aaron", "Company WiFi is out - Contact ISP", "High");
 buildSupportTicket("Mickey Mantle", "Need signature added to Outlook", "Low");
+
+// Task 3: Converting NodeLists to Arrays for Bulk Updates
+
+// these lines grab all of the elements with a certain class, then make that list an array
+const highPrioTickets = document.querySelectorAll(".highPriority");
+const highPrioArray = Array.from(highPrioTickets);
+
+// using a forEach method to go through each ticket and update the color and border
+highPrioArray.forEach(ticket => {
+    ticket.style.backgroundColor = "red";
+});
